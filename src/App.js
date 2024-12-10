6e62952b836158fdcb00a0349d447f73e7ea23ff
syedanwar1 <syedanwarsmarty@gmail.com>
2024-12-10 20:14:12 +0530

    messanger

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Header } from './messanger/Header';
+import Messanger from './messanger/Messanger';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Messanger />
+    </div>
+  );
+}
+
+export default App;
